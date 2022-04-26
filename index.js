3c95ebabe97fe96012749763392d9e74a0b8521e
Maxwell Bourgeois <maxwellbourgeois@gmail.com>
2022-04-26 08:30:36 -0500

    commit tree 9eadaaed2da8e757a3ce4accb68c1d70bbc221f6

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift()
+}
+
+function appendCat(name) {
+  const result = [...cats, name]
+  return result
+}
+
+function prependCat(name) {
+  const result = [name, ...cats]
+  return result
+}
+
+function removeLastCat() {
+  let result = [...cats]
+  result.pop()
+  return result
+}
+
+function removeFirstCat() {
+  let result = [...cats]
+  result.shift()
+  return result
+}
